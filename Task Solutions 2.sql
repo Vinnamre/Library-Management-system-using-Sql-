1f9c3421fab379599e57c843a9d33cf9ea844a6d
Yogesh Malik <152005024+Vinnamre@users.noreply.github.com>
2025-07-01 19:18:44 +0530

    Update Task Solutions 2.sql

--- Task Solutions 2.sql
+++ Task Solutions 2.sql
@@ -90,3 +90,15 @@ order by 1 asc, 2 asc;
 
 select * from branch_report;
 
+
+-- Task 16: CTAS: Create a Table of Active Members Use the CREATE TABLE AS (CTAS) statement to create a new table active_members containing members who have issued at least one book in the last 15 months.
+
+create table active_members
+as
+select * from members
+where member_id in (
+	select distinct issued_member_id 
+	from issued_status
+	where issued_date >= current_date - interval '15' month );
+
+select * from active_members;
